db76c6aa4cfc09f89de99c4610efb1d0d01ef396
Kristi Gribble <kgribble4083@gmail.com>
2020-04-28 20:28:53 -0500

    installed dependencies, initial commit, wrote test placeholder todos

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("enhancer function", () => {
+    describe("repair", () => {
+        it("should return an item with durability: 100", () => {
+            expect(repair(item)).toBe({ durability: 100 })
+            expect(repair(item)).notToBe({ durability: 100 })
+        })
+    })
+
+    describe("succeed", () => {
+        it.todo("should return a successful enhancement")
+    })
+
+    describe("fail", () => {
+        it.todo("should return a failed enhancement")
+    })
+
+    describe("get", () => {
+        it.todo("should return a modified name for the item reflecting the enhancement")
+    })
+
+})

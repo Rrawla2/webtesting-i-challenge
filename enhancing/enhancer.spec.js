b51aafc129b4b8556f610af30dab2017b9f78f1c
Kristi Gribble <kgribble4083@gmail.com>
2020-04-28 23:04:21 -0500

    added tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,20 +1,66 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
+
 // test away!
+const sword = {
+    name: "Holy Sword",
+    enhancement: 10,
+    durability: 10
+}
+
+const perfectSword = {
+    name: "Perfect Holy Sword",
+    enhancement: 20,
+    durability: 100
+}
+
+const imperfectSword = {
+    name: "Imperfect Holy Sword",
+    enhancement: 16,
+    durability: 80
+}
+
+const damagedSword = {
+    name: "Damaged Sword",
+    enhancement: 15,
+    durability: 60
+}
 
 describe("enhancer function", () => {
     describe("repair", () => {
         it("should return an item with durability: 100", () => {
-            expect(repair(item)).toBe({ durability: 100 })
-            expect(repair(item)).notToBe({ durability: 100 })
+            expect(repair(sword)).toEqual({ ...sword, durability: 100 })   
+        })
+
+        it("should not return an item with durability: 100", () => {
+            expect(repair(sword)).not.toEqual({ ...sword, durability: 100 })
         })
     })
 
-    describe("succeed", () => {
-        it.todo("should return a successful enhancement")
+    describe("success", () => {
+        it("should return a successful enhancement",  () => {
+            expect(succeed(sword)).toEqual({ ...sword, enhancement: 11})
+            expect(succeed(perfectSword)).toEqual(perfectSword)
+        })
     })
 
-    describe("fail", () => {
-        it.todo("should return a failed enhancement")
+    describe("failure", () => {
+        it("should decrease durability by 5 if enhancement is < 15", () => {
+            expect(fail(sword)).toEqual({ ...sword, durability: 5 })
+        })
+
+        it("should decrease durability by 10 if enhancement is >= 15", () => {
+            expect(fail(perfectSword)).toEqual({ ...perfectSword, durability: 90 })
+        })
+
+        it("should decrease enhancement level by 1 if enhancement is > 16", () => {
+            expect(fail(imperfectSword)).toEqual({ ...imperfectSword, enhancement: 16, durability: 70 })
+        })
+
+        it("should not increase enhancement", () => {
+            expect(fail(damagedSword)).toEqual({ ...damagedSword, enhancement: 18, durability: 60})
+        })
+
+
     })
 
     describe("get", () => {
